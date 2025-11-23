6b6fcf771e75c7b757526811491fdebe7ad34339
Wolfieelite <martetaylor333@gmail.com>
2025-11-23 15:09:31 -0800

    Added component for navigation items

--- src/components/Navigation.tsx
+++ src/components/Navigation.tsx
@@ -1,27 +1,29 @@
 import { Link } from '@tanstack/react-router'
 
+interface NavigationItemProp {
+  title: string;
+  url: string;
+}
+
 function Navigation() {
   return (
     <div className="navigation flex justify-center gap-5 mb-12 sticky top-0 z-[99] backdrop-blur-3xl">
       <div className="flex gap-3">
-        <div className="text-3xl">
-          <button className="link-container">
-            <Link className="capitalize" to="/">Home</Link>
-          </button>
-        </div>
-        <div className="text-3xl">
-          <button className="link-contianer">
-            <Link className="capitalize" to="/gallery">Gallery</Link>
-          </button>
-        </div>
-        <div className="text-3xl">
-          <button className="link-container">
-            <Link className="capitalize" to="/aboutme">About me</Link>
-          </button>
-        </div>
+        <NavigationItem title="home" url="/" />
+        <NavigationItem title="gallery" url="/gallery" />
+        <NavigationItem title="about me" url="/aboutme" />
       </div>
-    </div >
+    </div>
   )
 }
 
+function NavigationItem({ title, url }: NavigationItemProp) {
+  return (
+    <div className="text-2xl">
+      <button className="link-container">
+        <Link className="capitalize" to={url}>{title}</Link>
+      </button>
+    </div>
+  )
+}
 export default Navigation
