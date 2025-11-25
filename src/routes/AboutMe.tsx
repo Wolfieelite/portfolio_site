b38337a688189ce12d8b8a8c405b244f699372fd
Wolfieelite <martetaylor333@gmail.com>
2025-11-24 16:04:29 -0800

    Changed route captialize in tanstack createFIleROute

--- src/routes/AboutMe.tsx
+++ src/routes/AboutMe.tsx
@@ -1,6 +1,6 @@
 import { createFileRoute } from '@tanstack/react-router'
 import AboutMe from "../components/AboutMe"
-export const Route = createFileRoute('/AboutMe')({
+export const Route = createFileRoute('/aboutme')({
   component: RouteComponent,
 })
 
