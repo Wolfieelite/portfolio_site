da55cf9f4154925deda5677696ea14f4813addeb
Wolfieelite <martetaylor333@gmail.com>
2025-12-02 19:28:50 -0800

    Update index.tsx

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,6 +1,7 @@
-import AboutMe from '@/components/AboutMe'
+import Content from '@/components/Content'
 import Games from '@/components/Games'
 import { createFileRoute } from '@tanstack/react-router'
+import { uiImage } from '@/utilz/photos'
 
 export const Route = createFileRoute('/')({
   component: App,
@@ -9,8 +10,22 @@ export const Route = createFileRoute('/')({
 function App() {
   return (
     <>
-      <main className="mx-64 flex gap-8 flex-col main-content">
-        <Games />
+      <main id="smoothe-wrapper" className="flex gap-96 flex-col snap-y snap-mandatory">
+        <Content title="Game Assets">
+          <img className='w-full' src={uiImage.gameassetBanner.src} alt="" />
+        </Content>
+
+        <Content title="Tile maps">
+          <img className="w-full" src={uiImage.tilemapBanner.src} alt="" />
+        </Content>
+
+        <Content title="character design"></Content>
+
+        <Content title="interior design"></Content>
+
+        <Content title="Animation"></Content>
+
+        <Content title="Games"> </Content>
       </main>
     </>
   )
