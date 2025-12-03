71ef908e27cc663987e5832d0a88e0c378888dad
Wolfieelite <martetaylor333@gmail.com>
2025-12-02 19:27:39 -0800

    Added a section for each image for home

--- src/components/Content.tsx
+++ src/components/Content.tsx
@@ -1,8 +1,17 @@
-function Content({ children }: { children: React.ReactNode }) {
+import React from "react"
+
+interface ContentProp {
+  title: string
+  className: string
+  children?: React.ReactNode
+}
+
+function Content({ title, className, children }: ContentProp) {
   return (
-    <section className="">
+    <section className={`flex justify-center items-center cursor-pointer snap-center ${className}`} >
+      <h1 className="capitalize absolute text-5xl backdrop-blur-sm py-1.5">{title}</h1>
       {children}
-    </section>
+    </section >
   )
 }
 
