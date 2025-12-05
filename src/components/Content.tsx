76b3b34eb96ab776276368ca658532b828884f7e
Wolfieelite <martetaylor333@gmail.com>
2025-12-05 08:56:26 -0800

    Updated to take in a url to link to gallery

--- src/components/Content.tsx
+++ src/components/Content.tsx
@@ -1,17 +1,21 @@
+import { Link } from "@tanstack/react-router"
 import React from "react"
 
 interface ContentProp {
   title: string
   className: string
   children?: React.ReactNode
+  url: string
 }
 
-function Content({ title, className, children }: ContentProp) {
+function Content({ title, className, url, children }: ContentProp) {
   return (
-    <section className={`flex justify-center items-center cursor-pointer snap-center ${className}`} >
-      <h1 className="capitalize absolute text-5xl backdrop-blur-sm py-1.5">{title}</h1>
-      {children}
-    </section >
+    <Link to={url}>
+      <section className={`flex justify-center items-center cursor-pointer snap-center  ${className}`} >
+        <h1 className="capitalize absolute text-5xl backdrop-blur-sm py-1.5">{title}</h1>
+        {children}
+      </section >
+    </Link>
   )
 }
 
