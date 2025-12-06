7eb4b537869ed627b3999ef0ad3026d4184ba143
Wolfieelite <martetaylor333@gmail.com>
2025-12-05 21:39:45 -0800

    Updated to hide empty content

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,31 +1,42 @@
+import gsap from "gsap"
+import { ScrollTrigger } from "gsap/ScrollTrigger"
+import { useGSAP } from "@gsap/react"
 import Content from '@/components/Content'
-import Games from '@/components/Games'
 import { createFileRoute } from '@tanstack/react-router'
 import { uiImage } from '@/utilz/photos'
+import { useRef } from "react"
+
 
 export const Route = createFileRoute('/')({
   component: App,
 })
 
+
 function App() {
+  const contentRef = useRef(null)
+
   return (
     <>
-      <main id="smoothe-wrapper" className="flex gap-96 flex-col snap-y snap-mandatory">
-        <Content title="Game Assets">
+      <main id="smoothe-wrapper" className="flex gap-96 flex-col snap-y snap-mandatory" ref={contentRef}>
+        <Content className="content game-asset-content" title="Game Assets" url="/gallery/#64x64 pixle art">
           <img className='w-full' src={uiImage.gameassetBanner.src} alt="" />
         </Content>
 
-        <Content title="Tile maps">
+        <Content className="content tile-map-content" title="Tile maps">
           <img className="w-full" src={uiImage.tilemapBanner.src} alt="" />
         </Content>
 
-        <Content title="character design"></Content>
+        <Content className="content character-design-content hidden" title="character design">
+          <img className="w-full" src={uiImage.characterBanner.src} />
+        </Content>
 
-        <Content title="interior design"></Content>
+        <Content className="content interior-design-content hidden" title="interior design">
+          <img className="w-full" src={uiImage.interiorBanner.src} alt="" />
+        </Content>
 
-        <Content title="Animation"></Content>
+        <Content className="content animation-content hidden" title="Animation"></Content>
 
-        <Content title="Games"> </Content>
+        <Content className="content games-content hidden" title="Games"> </Content>
       </main>
     </>
   )
