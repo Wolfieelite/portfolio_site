0b56ca19565f2d77a70dbe06b9fc44a53c9c9b78
Wolfieelite <martetaylor333@gmail.com>
2025-12-10 23:25:21 -0800

    embedded sprite game

---
+++ src/routes/games.tsx
@@ -0,0 +1,10 @@
+function games() {
+  return (
+    <iframe src="https://scratch.mit.edu/projects/449500313/embed"
+      allowtransparency="true"
+      frameborder="0"
+      allowfullscreen></iframe>
+  )
+}
+
+export default games
